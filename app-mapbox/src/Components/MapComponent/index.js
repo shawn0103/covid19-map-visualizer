ca0eb5cd72095718ef3cca79ca35bbb79da59335
Shawn Heredia <spd5@hw.ac.uk>
2023-02-20 22:21:00 +0400

    JSON creator done
    Issue: Mapbox cant seem to render 2 data sources

--- app-mapbox/src/Components/MapComponent/index.js
+++ app-mapbox/src/Components/MapComponent/index.js
@@ -3,25 +3,15 @@ import './style.css';
 import {Map, Source, Layer} from 'react-map-gl';
 import { useState, useRef } from 'react';
 import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
-import testData from './covid19.json'
+import testData from './testData.json'
+import testDataAfghanistan from './testDataAfghanistan.json'
+import testDataAlbania from './testDataAlbania.json'
 const token = process.env.REACT_APP_MAPBOX_API_KEY;
 const fs = require('fs');
 
 function MapComponent() {
   
-  const updateData = () => {
-    let obj = JSON.parse(testData);
-    obj['features'].push({ "geometry": { "type": "Point", "coordinates": [ -151.58, 63.1016, 0.0 ] } });
-    let jsonStr = JSON.stringify(obj);
-    
-    fs.writeFile(testData, jsonStr, function writeJSON(err) {
-      if (err) return console.log(err);
-      console.log(jsonStr);
-      console.log('writing to ' + testData);
-    });
-  }
-  
-  
+ 
   const mapRef = React.useRef();
 
   const onClick = event => {
@@ -62,9 +52,21 @@ function MapComponent() {
             ref={mapRef}
           >
             <Source
-              id="earthquakes"
+              id="afghan"
               type="geojson"
-              data={testData}
+              data={testDataAfghanistan}
+              cluster={true}
+              clusterMaxZoom={14}
+              clusterRadius={50}
+            >
+              <Layer {...clusterLayer} />
+              <Layer {...clusterCountLayer} />
+              <Layer {...unclusteredPointLayer} />
+            </Source>
+            <Source
+              id="albania"
+              type="geojson"
+              data={testDataAlbania}
               cluster={true}
               clusterMaxZoom={14}
               clusterRadius={50}
